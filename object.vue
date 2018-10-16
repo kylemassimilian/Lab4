9f5e21f86c4b5e79289b48292839f0600028c2d7
Kyle Massimilian <Kyle@Kyles-MacBook-Pro-4.local>
2018-10-16 10:51:09 -0400

    new

--- object.vue
+++ object.vue
@@ -7,7 +7,19 @@
       </li>
     </ul>
   </div>
-  
+  <!-- I could not figure out the syntax for a conditional for posting comment --> 
+  <!-- <div v-if object = object.id
+  			post comment
+  	</div> 
+
+  	<div v-else>
+  		nothing
+
+  	</div> 
+
+  	method = POST 
+  --> 
+
 </template>
 
 <script>
